bad1ba7c29584aa052e795ca030c3bb69633a747
Nguyen Ngoc Son <65350169+nnson19pm@users.noreply.github.com>
2021-10-28 19:07:08 +0700

    fix_index CSS

--- LibraryManagement.UI/wwwroot/js/_Layout.js
+++ LibraryManagement.UI/wwwroot/js/_Layout.js
@@ -44,30 +44,31 @@ $('.modal').addEventListener('click', (e) => {
 
 //Form create, edit, detels, delete
 
-const date = $('.date-of-birth')
-function checkValue(str, max) {
-    if (str.charAt(0) !== '0' || str == '00') {
-        let num = parseInt(str);
-        if (isNaN(num) || num <= 0 || num > max) num = 1
-        str = num > parseInt(max.toString().charAt(0))
-            && num.toString().length == 1 ? '0' + num : num.toString()
-    };
-    return str
-};
-date.addEventListener('input', function (e) {
-    this.type = 'text'
-    let input = this.value
-    if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3)
-    let values = input.split('/').map(function (v) {
-        return v.replace(/\D/g, '')
-    });
-    if (values[0]) values[0] = checkValue(values[0], 12)
-    if (values[1]) values[1] = checkValue(values[1], 31)
-    let output = values.map(function (v, i) {
-        return v.length == 2 && i < 2 ? v + ' / ' : v
-    });
-    this.value = output.join('').substr(0, 14);
-});
+//const date = $('.date-of-birth')
+//function checkValue(str, max) {
+//    if (str.charAt(0) !== '0' || str == '00') {
+//        let num = parseInt(str);
+//        if (isNaN(num) || num <= 0 || num > max) num = 1
+//        str = num > parseInt(max.toString().charAt(0))
+//            && num.toString().length == 1 ? '0' + num : num.toString()
+//    };
+//    return str
+//};
+//date.addEventListener('input', function (e) {
+//    this.type = 'text'
+//    let input = this.value
+//    if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3)
+//    let values = input.split('/').map(function (v) {
+//        return v.replace(/\D/g, '')
+//    });
+//    if (values[0]) values[0] = checkValue(values[0], 12)
+//    if (values[1]) values[1] = checkValue(values[1], 31)
+//    let output = values.map(function (v, i) {
+//        return v.length == 2 && i < 2 ? v + ' / ' : v
+//    });
+//    this.value = output.join('').substr(0, 14);
+//});
+
 
 //window.onscroll = () => {
 //    const scroll = $('.form-header')
